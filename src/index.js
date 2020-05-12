39b2bea320600371a2dc92e1e03c7a57b13f5370
Jeandre-jpg <190025@virtualwindow.co.za>
2020-05-12 10:27:01 +0200

    //test

--- src/index.js
+++ src/index.js
@@ -17,3 +17,10 @@ ReactDOM.render(
 serviceWorker.unregister();
 
 //test
+
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+function App() {
+  return <Button color="primary">Hello World</Button>;
+}
