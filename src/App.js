2bc13b41bda330a6c30089ff76a05fb8ca97096a
Jeandre-jpg <190025@virtualwindow.co.za>
2020-05-12 23:06:06 +0200

    Added nav-bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
 import React from "react";
 import './App.css';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 
-export default function App() {
-  const name = "Jeandré";
-  const element = <h1>Hello, {name}</h1>;
+
+
+export default function SimpleMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <div className="App" style={{ color: "pink" }}>
-      <h1>Hello {name}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      {element}
-      {name}
+    <div>
+      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+        Open Menu
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
     </div>
   );
 }
 
 
+
+// export default function App() {
+//   const name = "Jeandré";
+//   const element = <h1>Hello, {name}</h1>;
+//   return (
+//     <div className="App" style={{ color: "pink" }}>
+//       <h1>Hello {name}</h1>
+//       <h2>Start editing to see some magic happen!</h2>
+//       {element}
+//       {name}
+//     </div>
+//   );
+// };
