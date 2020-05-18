dfd26dcfca83f73acaec1d53398b3060496c4b4a
Jeandre-jpg <190025@virtualwindow.co.za>
2020-05-19 00:53:11 +0200

    Update API settings

--- src/App.js
+++ src/App.js
@@ -3,3 +3,6 @@ import "./styles.css";
 
 export const RapidAPI = require('rapidapi-connect');
   
+fetch('https://rapidapi.com/priaid/api/symptom-checker/endpoints')
+  .then(response => response.json())
+  .then(data => console.log(data));
