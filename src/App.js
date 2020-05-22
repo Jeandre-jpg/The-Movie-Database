1de9af106dc3ec58c8ac6c94a6e63df87afc6368
Jeandre-jpg <190025@virtualwindow.co.za>
2020-05-22 08:38:40 +0200

    Got my display working!!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
 import React from "react";
-import "./styles.css";
+// import Button from '@material-ui/core/Button';
+// import Menu from '@material-ui/core/Menu';
+// import MenuItem from '@material-ui/core/MenuItem';
 
-export const RapidAPI = require('rapidapi-connect');
-  
-fetch('https://rapidapi.com/priaid/api/symptom-checker/endpoints')
-  .then(response => response.json())
-  .then(data => console.log(data));
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+
+// export default function SimpleMenu() {
+//   const [anchorEl, setAnchorEl] = React.useState(null);
+
+//   const handleClick = (event) => {
+//     setAnchorEl(event.currentTarget);
+//   };
+
+//   const handleClose = () => {
+//     setAnchorEl(null);
+//   };
+
+  // return (
+  //   <div className="App" style={{ color: "pink" }}>
+  //     <h1>Hello {name}</h1>
+  //     <h2>Start editing to see some magic happen!</h2>
+  //     {element}
+  //     {name}
+  //   <div>
+  //     <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+  //       Open Menu
+  //     </Button>
+  //     <Menu
+  //       id="simple-menu"
+  //       anchorEl={anchorEl}
+  //       keepMounted
+  //       open={Boolean(anchorEl)}
+  //       onClose={handleClose}
+  //     >
+  //       <MenuItem onClick={handleClose}>Profile</MenuItem>
+  //       <MenuItem onClick={handleClose}>My account</MenuItem>
+  //       <MenuItem onClick={handleClose}>Logout</MenuItem>
+  //     </Menu>
+  //   </div>
+  // );
