08dbf10b55edfea85fdb2f7fc77d8c2fa10137f8
Jeandre-jpg <190025@virtualwindow.co.za>
2020-05-24 14:10:56 +0200

    Revert "Api fully functional"
    This reverts commit 591ea75d593a12b610871e16057414ab32d1341d.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { BasicData } from "./components/BasicData";
 // var Component = React.Component;
 // var CanvasJSReact = require('./canvasjs.react');
 // var CanvasJS = CanvasJSReact.CanvasJS;
@@ -13,7 +12,6 @@ export default function App() {
   return (
     <div className="App">
       <h1>Welcome To World Health</h1>
-      <BasicData />
     </div>
   );
 }
