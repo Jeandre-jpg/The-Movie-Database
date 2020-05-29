2382ecf6b763c26a8cd90cd7490705f73eb15098
Jeandre-jpg <190025@virtualwindow.co.za>
2020-05-29 13:24:17 +0200

    Nav bar

--- src/App.js
+++ src/App.js
@@ -1,65 +1,86 @@
 import React from "react";
 import { BasicData } from "./components/BasicData";
-// var Component = React.Component;
-// var CanvasJSReact = require('./canvasjs.react');
-// var CanvasJS = CanvasJSReact.CanvasJS;
-// var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+import Sidebar from './components/Sidebar';
+var Component = React.Component;
+var CanvasJSReact = require('./canvasjs.react');
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
 // import Button from '@material-ui/core/Button';
 // import Menu from '@material-ui/core/Menu';
 // import MenuItem from '@material-ui/core/MenuItem';
 
 
-export default function App() {
+
+
+const items = [
+  { name: 'home', label: 'Home' },
+  {
+    name: 'billing',
+    label: 'Billing',
+    items: [
+      { name: 'statements', label: 'Statements' },
+      { name: 'reports', label: 'Reports' },
+    ],
+  },
+  {
+    name: 'settings',
+    label: 'Settings',
+    items: [{ name: 'profile', label: 'Profile' }],
+  },
+]
+
+function App() {
   return (
-    <div className="App">
-      <h1>Welcome To World Health</h1>
-      <BasicData />
+    <div>
+      <Sidebar items={items} />
     </div>
-  );
+  )
 }
 
+export default App
 
 
-// class Column extends Component {
-// 	render() {
-// 		const options = {
-// 			title: {
-// 				text: "Most Common Symptoms"
-// 			},
-// 			data: [
-// 			{
+class Column extends Component {
+	render() {
+		const options = {
+			title: {
+				text: "Most Common Symptoms"
+			},
+			data: [
+			{
 		
-//         type: "column",
-//         dataPoints: [
-//           { label: "Cough",  y: 10  },
-//           { label: "Chest pain", y: 15  },
-//           { label: "Shortness of breath", y: 25  },
-//           { label: "Wheezing",  y: 30  },
-//           { label: "Chest tightness",  y: 28  },
-//           { label: "Palpitations",  y: 32  },
-//           { label: "Nausea",  y: 35  },
-//           { label: "Heartburn",  y: 35  },
-//           { label: "Night cough",  y: 38  },
-//           { label: "Fast, deepened breathing",  y: 40  },
-//           { label: "Bloody cough",  y: 25  },
-//           { label: "Breathing-related pains",  y: 35  },
-//           { label: "Heart murmur",  y: 35  },
-//           { label: "Irregular heartbeat",  y: 38  },
+        type: "column",
+        dataPoints: [
+          { label: "Cough",  y: 10  },
+          { label: "Chest pain", y: 15  },
+          { label: "Shortness of breath", y: 25  },
+          { label: "Wheezing",  y: 30  },
+          { label: "Chest tightness",  y: 28  },
+          { label: "Palpitations",  y: 32  },
+          { label: "Nausea",  y: 35  },
+          { label: "Heartburn",  y: 35  },
+          { label: "Night cough",  y: 38  },
+          { label: "Fast, deepened breathing",  y: 40  },
+          { label: "Bloody cough",  y: 25  },
+          { label: "Breathing-related pains",  y: 35  },
+          { label: "Heart murmur",  y: 35  },
+          { label: "Irregular heartbeat",  y: 38  },
         
-// 				]
-// 			}
-// 			]
-// 		}
-// 		return (
-// 		<div>
-// 			<CanvasJSChart options = {options}
-// 				onRef={ref => this.chart = ref} 
-// 			/>
-// 			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
-// 		</div>
-// 		);
-// 	}
-// }
+				]
+			}
+			]
+		}
+		return (
+		<div>
+			<CanvasJSChart options = {options}
+				onRef={ref => this.chart = ref} 
+			/>
+			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+		</div>
+		);
+	}
+}
 // module.exports = Column;         
 
 // export default function SimpleMenu() {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from './App';
 
-import App from "./App";
+ReactDOM.render(<App />, document.getElementById('root'))
+
+
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
